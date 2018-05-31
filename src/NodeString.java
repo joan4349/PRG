03495cc0954cfcd73df8c2ff45f046d0a18c3426
Juvemanu <39400612+Juvemanu@users.noreply.github.com>
2018-05-31 13:05:22 +0200

    Update NodeString.java

--- src/NodeString.java
+++ src/NodeString.java
@@ -1,44 +1,37 @@
 
 public class NodeString {
 
-    public String word, traduction, description;//lo declaro public porque puedo
-	public int lines , timesAppearing;
-	public NodeString next;//variable referencia para apuntar objetos de tipo nodeString
-	//clase del nodo de enteros
-	
-	/**Constructor: 
-	 * @param, <b>String:</b> <i> dato del nodo.</i>*/
-	public NodeString(String d){
-		word = d;
-	    next = null;
-	}
-	
-	/**Constructor:
-	 * @param, <b>String:</b> <i> dato del nodo.</i>
-	 * @param, <b>NodeString:</b> <i> dato de la posicion next del nodo.</i>*/
-	public NodeString(String d, NodeString n) {
-		word = d;
-		next = n;
-	}
-	
-	/**Constructor:
-	 * @param, <b>String:</b> <i> dato del nodo.</i>
-	 * @param, <b>NodeString:</b> <i> dato de la posicion next del nodo.</i>*/
-	public NodeString(String w, String trad,String descr, NodeString n) {
-		word = w;
-		traduction = trad;
-		next = n;
-		description = descr;
-	}
-	
-	/**Constructor:
-	 * @param, <b>String:</b> <i> dato del nodo.</i>
-	 * @param, <b>NodeString:</b> <i> dato de la posicion next del nodo.</i>*/
-	public NodeString(String w, int lin, int timesA, NodeString n) {
-		word = w;
-		lines = lin;
-		next = n;
-		timesAppearing = timesA;
-	}
+    String word, traduction;//lo declaro public porque pued
+    QueueInt line;
+    
+    NodeString next;//variable referencia para apuntar objetos de tipo nodeString
+    //clase del nodo de enteros
+    
+    /**Constructor: 
+     * @param, <b>String:</b> <i> dato del nodo.</i>*/
+    public NodeString(String d){
+        word = d;
+        next = null;
+    }
+    
+    /**Constructor:
+     * @param, <b>String:</b> <i> dato del nodo.</i>
+     * @param, <b>NodeString:</b> <i> dato de la posicion next del nodo.</i>*/
+    public NodeString(String w, NodeString n) {
+        this.word = w;
+        this.traduction = "";
+        this.next = n;
+        this.line = new QueueInt();
+    }   
+    
+    /**Constructor:
+     * @param, <b>String:</b> <i> dato del nodo.</i>
+     * @param, <b>NodeString:</b> <i> dato de la posicion next del nodo.</i>*/
+        public NodeString(String w, String t, NodeString n) {
+        this.word = w;
+        this.traduction = t;
+        this.next = n;
+        this.line = null;
+    }
 	
 }
